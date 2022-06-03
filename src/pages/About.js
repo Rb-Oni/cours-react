5b6245b800a90cfd3fbc00ea8f001243cbb7b340
Robin FALCK <robin.falck57@gmail.com>
2022-06-03 13:14:18 +0200

    navigation

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const About = () => {
     return (
         <div>
+            <Navigation />
             <h1>A propos</h1>
             <br />
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nisi a quibusdam deserunt tempora dolore nesciunt, praesentium amet? Officia eos vitae laboriosam modi fugiat ut doloremque qui nemo incidunt aspernatur.</p>
