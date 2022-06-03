22a7be683b0c21b6ac3b511b46c224800b27a6b6
Robin FALCK <robin.falck57@gmail.com>
2022-06-03 13:00:36 +0200

    setup

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nisi a quibusdam deserunt tempora dolore nesciunt, praesentium amet? Officia eos vitae laboriosam modi fugiat ut doloremque qui nemo incidunt aspernatur.</p>
+        </div>
+    );
+};
+
+export default About;
